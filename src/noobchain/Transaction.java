8553a7a82f209616b9c8c68ff53478baa91bfc51
Nic <nicholas.groenewald@icloud.com>
2025-11-15 00:38:30 +0000

    Add files via upload

---
+++ src/noobchain/Transaction.java
@@ -0,0 +1,102 @@
+package noobchain;
+
+import java.security.*;
+import java.util.ArrayList;
+
+public class Transaction {
+
+    public String transactionId; //Contains a hash of transaction*
+    public PublicKey sender; //Senders address/public key.
+    public PublicKey reciepient; //Recipients address/public key.
+    public float value; //Contains the amount we wish to send to the recipient.
+    public byte[] signature; //This is to prevent anybody else from spending funds in our wallet.
+
+    public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+    public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+
+    private static int sequence = 0; //A rough count of how many transactions have been generated
+
+    // Constructor:
+    public Transaction(PublicKey from, PublicKey to, float value,  ArrayList<TransactionInput> inputs) {
+        this.sender = from;
+        this.reciepient = to;
+        this.value = value;
+        this.inputs = inputs;
+    }
+    // This calculates the transaction hash (which will be used as its id)
+    public void generateSignature(PrivateKey privateKey) {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+        signature = StringUtil.applyECDSASig(privateKey,data);
+    }
+
+    public boolean verifySignature() {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+        return StringUtil.verifyECDSASig(sender, data, signature);
+    }
+
+    //Verifies the data we signed hasnt been tampered with
+    private String calulateHash() {
+        sequence++; //increase the sequence to avoid 2 identical transactions having the same hash
+        return StringUtil.applySha256(
+                StringUtil.getStringFromKey(sender) +
+                        StringUtil.getStringFromKey(reciepient) +
+                        Float.toString(value) + sequence
+        );
+    }
+
+    public boolean processTransaction(){
+        if(verifySignature() == false){
+            System.out.println("#Transaction Signature failed to verify");
+            return false;
+        }
+
+        //gather transaction inputs (Make sure they are unspent):
+        for(TransactionInput i : inputs){
+            i.UTXO = NoobChain.UTXOs.get(i.transactionOutputId);
+        }
+
+        //check if transaction is valid:
+        if(getInputsValue() < NoobChain.minimumTransaction) {
+            System.out.println("#Transaction Inputs to small: " + getInputsValue());
+            return false;
+        }
+
+        //generate transaction outputs:
+        float leftOver = getInputsValue() - value; //get value of inputs then the left over change:
+        transactionId = calulateHash();
+        outputs.add(new TransactionOutput( this.reciepient, value,transactionId)); //send value to recipient
+        outputs.add(new TransactionOutput( this.sender, leftOver,transactionId)); //send the left over 'change' back to sender
+
+        //add outputs to Unspent List
+        for(TransactionOutput o : outputs){
+            NoobChain.UTXOs.put(o.id, o);
+        }
+
+        // remove transaction inputs from UTXO lists as spent
+        for(TransactionInput i : inputs){
+            if(i.UTXO == null) continue;
+            NoobChain.UTXOs.remove(i.UTXO.id);
+        }
+
+        return true;
+    }
+
+    // returns sum of inputs(UTXOs values)
+    public float getInputsValue(){
+        float total = 0;
+        for(TransactionInput i : inputs){
+            if(i.UTXO == null) continue; //if transaction cant be found skip it
+            total += i.UTXO.value;
+        }
+        return total;
+    }
+
+    // returns sum of outputs
+    public float getOutputsValue(){
+        float total = 0;
+        for(TransactionOutput o : outputs){
+            total += o.value;
+        }
+        return total;
+    }
+}
